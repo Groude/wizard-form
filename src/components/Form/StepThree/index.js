a14b3db1ebf754fdb194612e0ee968a6b401163e
Kirill Martynov <kirill_m@arkvision.pro>
2018-06-08 16:50:52 +0300

    Fixed props issue

--- src/components/Form/StepThree/index.js
+++ src/components/Form/StepThree/index.js
@@ -2,22 +2,24 @@ import React from 'react';
 
 class StepThree extends React.Component {
   render() {
+    const { id, location, departmentCode, dateId} = this.props.data
+
     return (
       <React.Fragment>
         <div className="form__group">
-          <input type="text" name="id" onChange={this.props.getValue} className="form__input" placeholder="Серия и Номер"/>
+          <input type="text" name="id" value={id} onChange={this.props.getValue} className="form__input" placeholder="Серия и Номер"/>
           <span className="input-name">Серия и Номер</span>
         </div>
         <div className="form__group">
-          <input type="text" name="location" onChange={this.props.getValue} className="form__input" placeholder="Кем выдан"/>
+          <input type="text" name="location" value={location} onChange={this.props.getValue} className="form__input" placeholder="Кем выдан"/>
           <span className="input-name">Кем выдан</span>
         </div>
         <div className="form__group">
-          <input type="text" name="departmentCode" onChange={this.props.getValue} className="form__input" placeholder="Код подразделения"/>
+          <input type="text" name="departmentCode" value={departmentCode} onChange={this.props.getValue} className="form__input" placeholder="Код подразделения"/>
           <span className="input-name">Код подразделения</span>
         </div>
         <div className="form__group">
-          <input type="text" name="dateId" onChange={this.props.getValue} className="form__input" placeholder="Дата выдачи"/>
+          <input type="text" name="dateId" value={dateId} onChange={this.props.getValue} className="form__input" placeholder="Дата выдачи"/>
           <span className="input-name">Дата выдачи</span>
         </div>
       </React.Fragment>
